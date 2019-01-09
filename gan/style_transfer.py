d23d41e8f1800e8fd209d3b116ca43a347ebf568
Angel Hernandez <ahernandez0691@gmail.com>
2019-01-08 23:50:28 -0800

    style transfer test

---
+++ gan/style_transfer.py
@@ -0,0 +1,18 @@
+from keras import backend as kb
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+inPath = "/Users/angel/Desktop/git/coding-and-cats/imgs/backgrounds/fry-code.jpeg"
+textureInPath = "texture.jpg"
+outPath = "img_out/ .jpg"
+
+targetHeight = 512
+targetWidth = 512
+targetSize = (targetHeight, targetWidth)
+
+inImage = load_img(path=inPath, target_size=targetSize)
+inArray = img_to_array(inImage)
+inArray = kb.variable(preprocess_input(np.expand_dims(inArray, axis=0)), dtype='float32')
+
+print(inArray.shape)
